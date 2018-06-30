7aa4bb29501a9d99a3a418970137548f111b08a8
Jorrit <jorrit@veire.com>
2018-06-30 11:11:21 +0200

    Fixed typo

---
+++ src/LiteAdmin/Extensions/TableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace LiteAdmin.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core;
+
+    public static class TableExtensions
+    {
+        public static ITable GetTableByName(this ICollection<ITable> tables, string tableName)
+        {
+            return tables?.FirstOrDefault(e => e.Name.Equals(tableName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
