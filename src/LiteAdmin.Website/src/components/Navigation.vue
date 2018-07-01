02ac40e97a21a79558a947a09c9958464c1a117a
Jorrit <jorrit@veire.com>
2018-07-01 20:03:42 +0200

    Some styling

--- src/LiteAdmin.Website/src/components/Navigation.vue
+++ src/LiteAdmin.Website/src/components/Navigation.vue
@@ -1,8 +1,14 @@
 <template>
-    <ul id="nav">
-        <li><router-link to="/">Home</router-link></li>
-        <li v-for="table in tables"><router-link :to="'/maintain/' + table.name">{{table.name}}</router-link></li>
-    </ul>
+    <div class="navigation">
+        <ul class="navigation__list">
+            <li class="navigation__item">
+                <router-link to="/">Home</router-link>
+            </li>
+            <li class="navigation__item" v-for="table in tables">
+                <router-link :to="'/maintain/' + table.name" class="navigation__link">{{table.name}}</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts" src="./Navigation.ts"></script>
